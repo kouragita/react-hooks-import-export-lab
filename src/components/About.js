ab42b9772a2297e58bbfb50fb3b8e743882fed4e
wilson mwangi waweru <kouragitam@gmail.com>
2024-07-31 03:09:11 +0300

    adds import-export

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { image } from "../data/user"; // Correct import
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I Made This</p>
+      <p>This is a brief about me.</p>
       <img src={image} alt="I made this" />
     </div>
   );
-}
+};
+export default About;
